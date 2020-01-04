985c95586c85cf3e6af530f75adefb949a341f69
Alisson Macedo <alissontecnico@gmail.com>
2020-01-04 03:05:17 -0300

    add page dashboard end new

---
+++ frontend/aircnc/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "./App.css";
+
+import Routes from "./routes";
+
+import logo from "./assets/logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <img src={logo} alt="Aircnc" />
+
+        <div className="content">
+          <Routes />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
